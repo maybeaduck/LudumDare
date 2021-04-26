a25237413a0db2625407ffd58fe9fd3038f9d129
s.korolev <korolev.s@cedargame.com>
2021-04-26 09:48:31 +0300

    Поменял префабы для уровня

---
+++ Assets/Level.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public int CountEnemies;
+    public bool Ready;
+    public Transform[] SpawnTransforms;
+}
