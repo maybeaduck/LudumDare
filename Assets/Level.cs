2dc2e7dcedae186f6389fb6a3a023a4248f7c745
s.korolev <korolev.s@cedargame.com>
2021-04-26 10:46:56 +0300

    делаю ui

--- Assets/Level.cs
+++ Assets/Level.cs
@@ -1,10 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    public GameObject[] Enemies;
     public int CountEnemies;
-    public bool Ready;
+
     public Transform[] SpawnTransforms;
+    public bool Ready;
+
+    private void Update()
+    {
+        if (Ready)
+        {
+            for (int i = 0; i < CountEnemies; i++)
+            {
+                var randomSpawnPoint = Random.Range(0, SpawnTransforms.Length);
+                var randomEnemy = Random.Range(0, Enemies.Length);
+
+                Instantiate(Enemies[randomEnemy],SpawnTransforms[randomSpawnPoint].position, SpawnTransforms[randomSpawnPoint].rotation);
+            }
+
+            Ready = false;
+        }
+    }
 }
